3c6bc87f1217987f2df89d7461ed67b2621578a4
Sebastian Weddmark Olsson <visnae@gmail.com>
2016-08-13 08:54:02 +0200

    Split packages

--- company-distel-pkg.el
+++ company-distel-pkg.el
@@ -1,20 +1,13 @@
-;;; distel-completion --- Erlang/distel completion backend
+;;; company-distel --- Erlang/distel completion backend for company-mode
 
 ;; Copyright (C) 2012 Sebastian Weddmark Olsson
 
 ;; Author:   Sebastian Weddmark Olsson
+;; Homepage: http://github.com/sebastiw/distel-completion
+;; Keywords: Erlang Distel company-mode
 ;; Created:  2012
-;; URL:      http://github.com/sebastiw/distel-completion
-;; Keywords: erlang distel auto-complete company-mode
-;; License:  BEER-WARE
 
 ;;; Commentary:
-;;
-;; Auto-complete-mode
-;; ------------------
-;; (require 'auto-complete)
-;; (require 'auto-complete-distel)
-;; (add-to-list 'ac-sources 'distel-completions)
 ;;
 ;; company-mode
 ;; ------------------
@@ -27,16 +20,19 @@
 ;; When set, the doc-buffer should use a popup instead of a whole buffer.
 ;; (setq erl-company-popup-help t)
 ;;
+;; Specifies the height of the popup created when `erl-company-popup-help' is set.
+;; (setq erl-company-popup-height 30)
+;;
 ;; Which syntax to skip backwards to find start of word.
 ;; (setq distel-completion-get-doc-from-internet t)
 ;;
 ;; Which syntax to skip backwards to find start of word.
 ;; (setq distel-completion-valid-syntax "a-zA-Z:_-")
 ;;
-;; Specifies the height of the popup created when `erl-company-popup-help' is set.
-;; (setq erl-company-popup-height 30)
-;;
 ;;; Code
 
-(define-package "distel-completion" "1.0.0"
-  "Erlang/distel completion backend for both auto-complete and company-mode")
+(define-package "company-distel" "1.0.0"
+  "Erlang/distel completion backend for company-mode"
+  '((company "0.9.0")))
+
+;;; company-distel ends here
